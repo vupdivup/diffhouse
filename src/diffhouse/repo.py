2520afb232b2f0f6f2a06c4d6b39445e71f1a2ff
vupdivup <vnmatt2003@gmail.com>
2025-09-17 11:13:36 +0200

    add repo column to all dataframes

--- src/diffhouse/repo.py
+++ src/diffhouse/repo.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from .cloning import SlimClone
-from .engine import get_commits, get_branches, get_tags
+from .engine import get_remote_url, get_commits, get_branches, get_tags
 
 class Repo:
     '''
@@ -9,17 +9,20 @@ class Repo:
     '''
     def __init__(self, url: str):
         '''
-        Initialize the repository and load commit history.
+        Initialize the repository and load metadata. This may take some time
+        depending on the repository size.
 
         Args:
             url (str): URL of the repository.
         '''
-        self._url = url
 
         with SlimClone(url) as c:
-            self._commits = get_commits(c.path)
-            self._branches = get_branches(c.path)
-            self._tags = get_tags(c.path)
+            # get normalized URL via git
+            self._url = get_remote_url(c.path)
+
+            self._commits = get_commits(c.path).assign(repository=self.url)
+            self._branches = get_branches(c.path).assign(repository=self.url)
+            self._tags = get_tags(c.path).assign(repository=self.url)
 
     @property
     def url(self):
