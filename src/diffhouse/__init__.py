ddb264700782c9428d290e31cc1a52feda94dcc0
vupdivup <vnmatt2003@gmail.com>
2025-09-22 20:42:35 +0200

    use config.py instead of logging.yml for log config

--- src/diffhouse/__init__.py
+++ src/diffhouse/__init__.py
@@ -1,13 +1,8 @@
-import yaml
-import logging
 import logging.config
-from importlib import resources
 
 from .repo import Repo
-from .constants import PACKAGE_NAME
+from . import config
 
-with (resources.files(PACKAGE_NAME) / 'static/logging.yml').open('r') as f:
-    config = yaml.safe_load(f)
-    logging.config.dictConfig(config)
+logging.config.dictConfig(config.LOGGING)
 
 __all__ = ['Repo']
