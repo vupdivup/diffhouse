97e73987cafc69b439397d6f95be401ba80ff16f
vupdivup <vnmatt2003@gmail.com>
2025-10-03 22:48:53 +0200

    rewrite tests

---
+++ tests/constants.py
@@ -0,0 +1,29 @@
+# taken from the list of most starred repos on GitHub, with commits in the
+# 10-20k range
+REPOS = [
+    'https://github.com/facebook/react',
+    'https://github.com/vuejs/vue',
+    'https://github.com/Significant-Gravitas/AutoGPT',
+    'https://github.com/twbs/bootstrap',
+    'https://github.com/n8n-io/n8n',
+    'https://github.com/yt-dlp/yt-dlp',
+    'https://github.com/Genymobile/scrcpy',
+    'https://github.com/langflow-ai/langflow',
+    'https://github.com/langchain-ai/langchain',
+    'https://github.com/d3/d3',
+    'https://github.com/axios/axios',
+    'https://github.com/microsoft/terminal',
+    'https://github.com/ohmyzsh/ohmyzsh',
+    'https://github.com/tailwindlabs/tailwindcss',
+    'https://github.com/ollama/ollama',
+    'https://github.com/microsoft/PowerToys',
+    'https://github.com/excalidraw/excalidraw',
+    'https://github.com/fatedier/frp',
+    'https://github.com/rustdesk/rustdesk',
+    'https://github.com/tauri-apps/tauri',
+    'https://github.com/fastapi/fastapi',
+]
+
+VALID_URL = 'https://github.com/vupdivup/diffhouse'
+
+INVALID_URL = 'yh8sxKcLRFS14zEa6PvNNPaGMzZA3l'
--- tests/test_repo.py
+++ tests/test_repo.py
@@ -1,77 +1,169 @@
+import random
+from datetime import datetime as dt
+from datetime import timezone as tz
+
+import polars as pl
 import pytest
-import pandas as pd
 
 from diffhouse import Repo
 
-REPOS = [
-    'https://github.com/ohmyzsh/ohmyzsh',
-    'https://github.com/tailwindlabs/tailwindcss',
-    'https://github.com/ollama/ollama',
-    'https://github.com/microsoft/PowerToys',
-    'https://github.com/excalidraw/excalidraw'
-]
-VALID_URL = 'https://github.com/vupdivup/diffhouse'
-INVALID_URL = 'yh8sxKcLRFS14zEa6PvNNPaGMzZA3l'
+from . import github
+from .constants import INVALID_URL, REPOS, VALID_URL
 
-@pytest.fixture(scope='module', params=REPOS)
-def repo(request):
-    return Repo(request.param, blobs=True)
+# randomly select 4 repos for testing to keep it relatively short
+SELECTED_REPOS = random.sample(REPOS, 4)
 
-@pytest.mark.parametrize('attr,col', [
-    ('commits', 'commit_hash'),
-    ('branches', 'branch'),
-    ('tags', 'tag')])
-def test_primary_key(repo, attr, col):
-    '''
-    Test that `repo.attr` has a valid primary key in column `col`.
-    '''
-    df = getattr(repo, attr)
-    assert col in df.columns
-    assert df[col].notna().all()
-    assert df[col].is_unique
 
-@pytest.mark.parametrize('attr', ['commits', 'branches', 'tags', 'diffs'])
-def test_nulls(repo, attr):
-    '''
-    Test that `repo.attr` has no fully null rows.
-    '''
-    df = getattr(repo, attr)
-    assert len(df.replace("", pd.NA).dropna(how='all')) == len(df)
+@pytest.fixture(scope='module', params=SELECTED_REPOS)
+def repo(request) -> Repo:
+    """Fixture that provides a `Repo` instance for a given GitHub URL."""
+    return Repo(request.param, blobs=True).load()
 
-@pytest.mark.parametrize('attr', ['commits', 'branches', 'diffs'])
-def test_not_empty(repo, attr):
-    '''
-    Test that `repo.attr` is not empty.
-    '''
-    df = getattr(repo, attr)
-    assert not df.empty
 
-def test_diff_matches(repo):
-    '''
-    Tests that the join of status changes and line changes to form diffs is
-    correct.
-    '''
-    assert len(repo.diffs) == len(repo._status_changes)
-    assert len(repo.diffs) == len(repo._line_changes)
+def test_branches(repo: Repo):
+    """Test that an extract of GitHub branches matches `repo.branches`."""
+    branches_gh = [b['name'] for b in github.get_branches(repo.url)]
 
-@pytest.mark.parametrize('df', ['commits', 'branches', 'tags', 'diffs'])
-def test_source_col(repo, df):
-    '''
-    Test that `repo.attr` has a `repository` column.
-    '''
-    assert 'repository' in getattr(repo, df).columns
+    for branch in branches_gh:
+        assert branch in repo.branches
+
+
+def test_tags(repo: Repo):
+    """Test that an extract of GitHub tags matches `repo.tags`."""
+    tags_gh = [t['name'] for t in github.get_tags(repo.url)]
+
+    for tag in tags_gh:
+        assert tag in repo.tags
+
+
+def test_commits_vs_github(repo: Repo):
+    """Test that an extract of commits from GitHub matches `repo.commits`."""
+    commits_gh = github.get_commits(repo.url)
+
+    commits_df = pl.DataFrame(repo.commits)
+
+    for commit_gh in commits_gh:
+        commit_hash_gh = commit_gh['sha']
+        commit_local = commits_df.filter(
+            pl.col('commit_hash') == commit_hash_gh
+        )
+
+        assert not commit_local.is_empty()
+
+        commit_local = commit_local.row(0, named=True)
+
+        assert (
+            commit_local['author_name'] == commit_gh['commit']['author']['name']
+        )
+        assert (
+            commit_local['author_email']
+            == commit_gh['commit']['author']['email']
+        )
+        assert (
+            commit_local['committer_name']
+            == commit_gh['commit']['committer']['name']
+        )
+        assert (
+            commit_local['committer_email']
+            == commit_gh['commit']['committer']['email']
+        )
+
+        # GitHub API line endings are not normalized it seems
+        gh_message_clean = commit_gh['commit']['message'].replace('\r\n', '\n')
+
+        assert commit_local['subject'] in gh_message_clean
+        assert commit_local['body'] in gh_message_clean
+
+        # GitHub commit date is in UTC by default
+        commit_date_gh = dt.fromisoformat(commit_gh['commit']['author']['date'])
+
+        commit_date_local = dt.fromisoformat(
+            commit_local['author_date']
+        ).astimezone(tz.utc)
+
+        assert commit_date_local == commit_date_gh
+
+
+def test_lines_changed__commits_vs_files(repo: Repo):
+    """Test that the number of line changes in `repo.commits` matches `repo.changed_files`."""
+    commits = pl.DataFrame(repo.commits)
+    changed_files = pl.DataFrame(repo.changed_files)
+
+    files_grouped = changed_files.group_by('commit_hash').agg(
+        pl.sum('lines_added'), pl.sum('lines_deleted')
+    )
+
+    joined = commits.join(files_grouped, on='commit_hash', how='left').fill_nan(
+        0
+    )
+
+    assert joined.filter(
+        (pl.col('lines_added') != pl.col('lines_added_right'))
+        | (pl.col('lines_deleted') != pl.col('lines_deleted_right'))
+    ).is_empty()
+
+
+def test_files_changed__commits_vs_files(repo):
+    """Test that the number of files changed in `repo.commits` matches `repo.changed_files`."""
+    commits = pl.DataFrame(repo.commits).select('commit_hash', 'files_changed')
+    changed_files = pl.DataFrame(repo.changed_files).select('commit_hash')
+
+    files_grouped = changed_files.group_by('commit_hash').agg(
+        pl.len().alias('files_changed')
+    )
+
+    joined = commits.join(files_grouped, on='commit_hash', how='left').fill_nan(
+        0
+    )
+
+    errors = joined.filter(
+        (pl.col('files_changed') != pl.col('files_changed_right'))
+    )
+
+    assert errors.is_empty()
+
+
+def test_lines_changed__files_vs_diffs(repo: Repo):
+    """Test that the number of line changes in `repo.changed_files` matches `repo.diffs`."""
+    changed_files = pl.DataFrame(repo.changed_files).select(
+        'changed_file_id', 'lines_added', 'lines_deleted'
+    )
+
+    diffs = pl.DataFrame(repo.diffs).select(
+        'changed_file_id', 'lines_added', 'lines_deleted'
+    )
+
+    diffs_grouped = diffs.group_by('changed_file_id').agg(
+        pl.sum('lines_added'), pl.sum('lines_deleted')
+    )
+
+    joined = changed_files.join(diffs_grouped, on='changed_file_id', how='left')
+
+    errors = joined.filter(
+        (pl.col('lines_added') != pl.col('lines_added_right'))
+        | (pl.col('lines_deleted') != pl.col('lines_deleted_right'))
+    )
+
+    # allow a bit of wiggle room as line change counts can be different for
+    # `git log shortstat` and `git log -p`
+    assert len(errors) / len(joined) < 0.01
+
 
 def test_invalid_url():
-    '''
-    Test that initializing `Repo` with an invalid URL raises an exception.
-    '''
+    """Test that initializing `Repo` with an invalid URL raises an exception."""
     with pytest.raises(Exception):
-        Repo(INVALID_URL)
+        Repo(INVALID_URL).load()
+
 
 def test_no_blobs():
-    '''
-    Test that initializing `Repo` with `blobs`=`False` does not load diffs.
-    '''
-    r = Repo(VALID_URL, blobs=False)
+    """Test that initializing `Repo` with `blobs`=`False` does not load diffs."""
+    r = Repo(VALID_URL, blobs=False).load()
+    with pytest.raises(ValueError):
+        r.changed_files
+
     with pytest.raises(ValueError):
         r.diffs
+
+
+if __name__ == '__main__':
+    pytest.main()
