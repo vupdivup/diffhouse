1bfebbace952b01e5654e63a3777692d1047912f
vupdivup <vnmatt2003@gmail.com>
2025-10-03 14:59:15 +0200

    configure Repo.load

--- src/diffhouse/repo.py
+++ src/diffhouse/repo.py
@@ -1,5 +1,4 @@
 from collections.abc import Iterator
-from functools import wraps
 
 from .cloning import TempClone
 from .engine import (
@@ -32,6 +31,13 @@ class Repo:
         self._blobs = blobs
         self._location = location.strip()
         self._active = False
+        self._loaded = False
+
+        self._branches = None
+        self._tags = None
+        self._commits = None
+        self._changed_files = None
+        self._diffs = None
 
     def __enter__(self):
         self._clone = TempClone(self.url, shallow=not self._blobs)
@@ -43,19 +49,34 @@ class Repo:
         self._clone.__exit__(exc_type, exc_value, traceback)
         self._active = False
 
-    def _check_active(func):
+    def load(self):
+        """Load all repository data into memory.
+
+        This is a convenience method to access properties without the `with`
+        statement. Not recommended for large repositories.
+        """
+        self.__enter__()
+
+        # load properties via getters
+        self.commits
+        self.branches
+        self.tags
+
+        if self._blobs:
+            self.changed_files
+            self.diffs
+
+        self.__exit__(None, None, None)
+
+        return self
+
+    def _raise_if_inactive(self):
         """Raise an error if the Repo context manager is inactive."""
-
-        @wraps(func)
-        def wrapper(self):
-            if not self._active:
-                raise RuntimeError(
-                    f'{Repo.__name__} object is not active.'
-                    " Wrap in a 'with' statement to query."
-                )
-            return func(self)
-
-        return wrapper
+        if not self._active:
+            raise RuntimeError(
+                f'{Repo.__name__} object is not active.'
+                " Wrap in a 'with' statement to query."
+            )
 
     @property
     def url(self) -> str:
@@ -64,41 +85,59 @@ class Repo:
         return self._location
 
     @property
-    @_check_active
     def branches(self) -> Iterator[str]:
         """Branch names of the repository."""
-        return get_branches(self._clone.path)
+        if not self._branches:
+            self._raise_if_inactive()
+            self._branches = list(get_branches(self._clone.path))
+        return self._branches
 
     @property
-    @_check_active
     def tags(self) -> Iterator[str]:
         """Tag names of the repository."""
-        return get_tags(self._clone.path)
+        if not self._tags:
+            self._raise_if_inactive()
+            self._tags = list(get_tags(self._clone.path))
+        return self._tags
 
     @property
-    @_check_active
     def commits(self) -> Iterator[Commit]:
         """Main branch commit history."""
-        return collect_commits(self._clone.path)
+        if not self._commits:
+            self._raise_if_inactive()
 
-    # TODO: find a better name
+            if self._blobs:
+                self._commits = list(
+                    collect_commits(self._clone.path, shortstats=True)
+                )
+            else:
+                self._commits = list(
+                    collect_commits(self._clone.path, shortstats=False)
+                )
+
+        return self._commits
 
     @property
-    @_check_active
     def changed_files(self) -> Iterator[ChangedFile]:
         """Files changed per commit."""
         if not self._blobs:
             raise ValueError(
                 'Initialize Repo with `blobs`=`True` to load changed files.'
             )
-        return collect_changed_files(self._clone.path)
+        if not self._changed_files:
+            self._raise_if_inactive()
+            self._changed_files = list(collect_changed_files(self._clone.path))
+
+        return self._changed_files
 
     @property
-    @_check_active
     def diffs(self) -> Iterator[Diff]:
         """Line-level changes within a commit."""
         if not self._blobs:
             raise ValueError(
                 'Initialize Repo with `blobs`=`True` to load diffs.'
             )
-        return collect_diffs(self._clone.path)
+        if not self._diffs:
+            self._raise_if_inactive()
+            self._diffs = list(collect_diffs(self._clone.path))
+        return self._diffs
