d8e8bcbdb733f6e71ac4c90c4af9b84470700d68
vupdivup <vnmatt2003@gmail.com>
2025-10-06 19:35:35 +0200

    write split_stream function

--- src/diffhouse/engine/utils.py
+++ src/diffhouse/engine/utils.py
@@ -1,4 +1,6 @@
 import hashlib
+from collections.abc import Iterator
+from io import StringIO
 
 from .constants import UNIT_SEPARATOR
 
@@ -20,3 +22,36 @@ def tweak_git_iso_datetime(dt: str) -> str:
 
     """
     return dt[:10] + 'T' + dt[11:19] + dt[20:23] + ':' + dt[-2:]
+
+
+def split_stream(
+    f: StringIO, sep: str, chunk_size: int = 1024
+) -> Iterator[str]:
+    """Split a stream into parts based on a separator.
+
+    Args:
+        f: Input stream to read from.
+        sep: Separator string to split the stream.
+        chunk_size: Number of characters to read at a time.
+
+    Yields:
+        Parts of the stream split by the separator.
+
+    """
+    buffer = ''
+
+    while True:
+        chunk = f.read(chunk_size)
+        if not chunk:
+            # EOF
+            if buffer:
+                yield buffer
+            break
+
+        buffer += chunk
+
+        if sep in buffer:
+            parts = buffer.split(sep)
+            for part in parts[:-1]:
+                yield part
+            buffer = parts[-1]
