99a6b5508a664833736a2386a04d258b213cdebc
vupdivup <vnmatt2003@gmail.com>
2025-10-07 20:18:38 +0200

    adjust Repo properties

--- src/diffhouse/engine/__init__.py
+++ src/diffhouse/engine/__init__.py
@@ -1,18 +1,18 @@
 """Git command parsers and data structures."""
 
 from .branches import get_branches
-from .changed_files import ChangedFile, collect_changed_files
-from .commits import Commit, collect_commits
-from .diffs import Diff, collect_diffs
+from .changed_files import ChangedFile, stream_changed_files
+from .commits import Commit, stream_commits
+from .diffs import Diff, stream_diffs
 from .tags import get_tags
 
 __all__ = [
     'Commit',
-    'collect_commits',
+    'stream_commits',
     'ChangedFile',
-    'collect_changed_files',
+    'stream_changed_files',
     'Diff',
-    'collect_diffs',
+    'stream_diffs',
     'get_branches',
     'get_tags',
 ]
