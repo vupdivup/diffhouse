162bf01752a6d249ccaf0e1857c2f7c1db006bc9
vupdivup <vnmatt2003@gmail.com>
2025-10-10 16:13:03 +0200

    write random sampling function for GitHub API

--- tests/github.py
+++ tests/github.py
@@ -1,7 +1,14 @@
+import math
 import os
+import random
+import re
 import warnings
+from collections.abc import Iterator
+from typing import Literal
 
 import requests
+
+PER_PAGE_LIMIT = 100
 
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 
@@ -9,30 +16,7 @@ if not GITHUB_TOKEN:
     warnings.warn('GITHUB_TOKEN environment variable not set.')
 
 
-def get_github_response(url: str, per_page: int = 50) -> requests.Response:
-    """Get a response from the GitHub API.
-
-    Args:
-        url: The API endpoint URL.
-        per_page: The number of items to return per page.
-
-    Returns:
-        response: The response object from the API call.
-
-    """
-    headers = {}
-    params = {'per_page': per_page}
-
-    if GITHUB_TOKEN:
-        headers['Authorization'] = f'Bearer {GITHUB_TOKEN}'
-
-    response = requests.get(url, headers=headers, params=params)
-    response.raise_for_status()
-
-    return response
-
-
-def extract_repo_info(repo_url) -> tuple[str, str]:
+def extract_repo_info(repo_url: str) -> tuple[str, str]:
     """Extract owner and repository name from a GitHub URL.
 
     Args:
@@ -40,7 +24,7 @@ def extract_repo_info(repo_url) -> tuple[str, str]:
 
     Returns:
         owner: Owner of the repository.
-        repo: Name of the repository. DOESNT WORK
+        repo: Name of the repository.
 
     """
     if repo_url.endswith('/'):
@@ -53,51 +37,75 @@ def extract_repo_info(repo_url) -> tuple[str, str]:
     return owner, repo
 
 
-def get_branches(repo_url) -> list[dict]:
-    """Fetch branches from a GitHub repository.
+def get_github_response(url: str, headers={}, params={}) -> requests.Response:
+    """Get a response from the GitHub API.
+
+    Args:
+        url: The API endpoint URL.
+        headers: Optional headers to include in the request.
+        params: Optional query parameters to include in the request.
+
+    Returns:
+        response: The response object from the API call.
+
+    """
+    headers = {**headers}
+    params = {**params}
+
+    params['per_page'] = PER_PAGE_LIMIT
+
+    if GITHUB_TOKEN:
+        headers['Authorization'] = f'Bearer {GITHUB_TOKEN}'
+
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+
+    return response
+
+
+def sample_github_endpoint(
+    repo_url: str,
+    entity: Literal['commits', 'branches', 'tags'],
+    n: int,
+    headers: dict = {},
+    params: dict = {},
+) -> Iterator[dict]:
+    """Sample at least n items from a GitHub repository endpoint, in a semi-random manner.
 
     Args:
         repo_url: URL of the GitHub repository.
+        entity: The entity to fetch (e.g., 'branches', 'tags', 'commits').
+        n: Minimum number of items to sample. Actual number is determined by
+            pagination.
+        headers: Optional headers to include in the request.
+        params: Optional query parameters to include in the request.
 
-    Returns:
-        branches: List of branches in the repository.
-
-    """
-    owner, repo = extract_repo_info(repo_url)
-    url = f'https://api.github.com/repos/{owner}/{repo}/branches'
-    response = get_github_response(url)
-
-    return response.json()
-
-
-def get_tags(repo_url) -> list[dict]:
-    """Fetch tags from a GitHub repository.
-
-    Args:
-        repo_url: URL of the GitHub repository.
-
-    Returns:
-        tags: List of tags in the repository.
+    Yields:
+        An item from the specified entity.
 
     """
     owner, repo = extract_repo_info(repo_url)
-    url = f'https://api.github.com/repos/{owner}/{repo}/tags'
-    response = get_github_response(url)
+    url = f'https://api.github.com/repos/{owner}/{repo}/{entity}'
 
-    return response.json()
+    resp = get_github_response(url, headers, params)
+    yield from resp.json()
 
+    link = resp.headers.get('Link', None)
 
-def get_commits(repo_url) -> list[dict]:
-    """Fetch commits from a GitHub repository.
+    # if there's no Link header, there's only one page of results
+    if not link:
+        return
 
-    Args:
-        repo_url: URL of the GitHub repository.
+    last_page = int(re.search(r'page=(\d+)>; rel="last"', link).group(1))
 
-    Returns:
-        commits: List of commits in the repository.
+    pages_left = math.ceil(n / PER_PAGE_LIMIT) - 1
 
-    """
-    owner, repo = extract_repo_info(repo_url)
-    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
-    response = get_github_response(url)
-    return response.json()
+    # randomly select pages
+    pages = random.sample(
+        range(2, last_page + 1), min(pages_left, last_page - 1)
+    )
+
+    for p in pages:
+        yield from get_github_response(
+            url, headers, {**params, 'page': p}
+        ).json()
