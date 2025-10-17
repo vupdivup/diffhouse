7fb701298bf1effefcbaec4b2dd6d145cf31a649
vupdivup <vnmatt2003@gmail.com>
2025-10-17 16:39:06 +0200

    fix(#98): use more obscure separators for git log output to avoid collisions

--- src/diffhouse/pipelines/constants.py
+++ src/diffhouse/pipelines/constants.py
@@ -1,2 +1,4 @@
-UNIT_SEPARATOR = '\ue001'
-RECORD_SEPARATOR = '\ue000'
+# these characters were randomly selected from the Unicode Supplementary Private
+# Use Area B to avoid collisions with any real text data
+UNIT_SEPARATOR = '\U00100808'
+RECORD_SEPARATOR = '\U001044f6'
