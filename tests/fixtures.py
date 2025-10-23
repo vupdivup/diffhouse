4aecf7ed087055e9470a2be70d889f64a423c7f1
vupdivup <vnmatt2003@gmail.com>
2025-10-23 08:41:55 +0200

    test: refactor fixtures.py to adhere to new API

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,4 +1,5 @@
 import random
+from typing import Iterator
 
 import polars as pl
 import pytest
@@ -17,30 +18,29 @@ SELECTED_REPOS = random.sample(REPOS, 3)
 
 
 @pytest.fixture(scope='session', params=SELECTED_REPOS)
-def repo(request: pytest.FixtureRequest) -> Repo:
+def repo(request: pytest.FixtureRequest) -> Iterator[Repo]:
     """Fixture that provides a `Repo` instance for a given GitHub URL."""
-    return Repo(request.param, blobs=True).load()
+    r = Repo(request.param, blobs=True).clone()
+    yield r
+    r.dispose()
 
 
 @pytest.fixture(scope='session')
 def commits__diffhouse(repo: Repo) -> pl.DataFrame:
-    """Fixture that provides a DataFrame of `repo.commits`.
-
-    Datetime strings are converted to objects.
-    """
-    return pl.DataFrame(repo.commits)
+    """Fixture that provides a DataFrame of `repo.commits`."""
+    return repo.commits.pl()
 
 
 @pytest.fixture(scope='session')
-def changed_files__diffhouse(repo: Repo) -> pl.DataFrame:
-    """Fixture that provides a DataFrame of `repo.changed_files`."""
-    return pl.DataFrame(repo.changed_files)
+def filemods__diffhouse(repo: Repo) -> pl.DataFrame:
+    """Fixture that provides a DataFrame of `repo.filemods`."""
+    return repo.filemods.pl()
 
 
 @pytest.fixture(scope='session')
 def diffs__diffhouse(repo: Repo) -> pl.LazyFrame:
     """Fixture that provides a DataFrame of `repo.diffs`."""
-    return pl.DataFrame(repo.diffs).lazy()
+    return repo.diffs.pl().lazy()
 
 
 @pytest.fixture(scope='session')
@@ -64,7 +64,7 @@ def commits__github(repo: Repo) -> list[dict]:
 
 
 @pytest.fixture(scope='session')
-def changed_files__github(
+def filemods__github(
     repo: Repo, commits__diffhouse: pl.DataFrame
 ) -> list[dict]:
     """Fixture that provides a list of file changes from the GitHub API.
