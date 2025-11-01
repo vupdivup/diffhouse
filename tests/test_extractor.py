9cc890eca4cdd7b5ac5861be59d40687482261a9
vupdivup <vnmatt2003@gmail.com>
2025-11-01 10:50:00 +0100

    fix: import repo fixture

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -7,6 +7,7 @@ from tests.constants import (
     SCHEMAS_BY_OBJECT_TYPE,
     TYPED_SCHEMAS_BY_OBJECT_TYPE,
 )
+from tests.fixtures import repo  # noqa: F401
 
 
 def assert_branch_is_valid(b: Branch) -> None:
@@ -95,7 +96,7 @@ def assert_dict_matches_typed_schema(item: dict, schema: dict) -> None:
             assert isinstance(item[field], field_type)
 
 
-def test_iter(repo: Repo) -> None:
+def test_iter(repo: Repo) -> None:  # noqa: F811
     """Test that items can be iterated and are valid."""
     for attr, type_ in OBJECT_TYPES_BY_REPO_ATTR.items():
         assertor = ASSERT_FUNCS_BY_OBJECT_TYPE[type_]
@@ -103,7 +104,7 @@ def test_iter(repo: Repo) -> None:
             assertor(item)
 
 
-def test_to_list(repo: Repo) -> None:
+def test_to_list(repo: Repo) -> None:  # noqa: F811
     """Test that items can be converted to a list and are valid."""
     for attr, type_ in OBJECT_TYPES_BY_REPO_ATTR.items():
         assertor = ASSERT_FUNCS_BY_OBJECT_TYPE[type_]
@@ -111,7 +112,7 @@ def test_to_list(repo: Repo) -> None:
             assertor(item)
 
 
-def test_iter_dicts(repo: Repo) -> None:
+def test_iter_dicts(repo: Repo) -> None:  # noqa: F811
     """Test that items can be iterated as dicts with the correct schema."""
     for attr, type_ in OBJECT_TYPES_BY_REPO_ATTR.items():
         schema = TYPED_SCHEMAS_BY_OBJECT_TYPE[type_]
@@ -119,7 +120,7 @@ def test_iter_dicts(repo: Repo) -> None:
             assert_dict_matches_typed_schema(item, schema)
 
 
-def test_to_dicts(repo: Repo) -> None:
+def test_to_dicts(repo: Repo) -> None:  # noqa: F811
     """Test that items can be converted to a dict with the correct schema."""
     for attr, type_ in OBJECT_TYPES_BY_REPO_ATTR.items():
         schema = TYPED_SCHEMAS_BY_OBJECT_TYPE[type_]
@@ -127,7 +128,7 @@ def test_to_dicts(repo: Repo) -> None:
             assert_dict_matches_typed_schema(item, schema)
 
 
-def test_to_pandas(repo: Repo) -> None:
+def test_to_pandas(repo: Repo) -> None:  # noqa: F811
     """Test conversion to pandas DataFrames."""
     for attr, type_ in OBJECT_TYPES_BY_REPO_ATTR.items():
         schema = SCHEMAS_BY_OBJECT_TYPE[type_]
@@ -136,7 +137,7 @@ def test_to_pandas(repo: Repo) -> None:
         assert set(df.columns) == schema
 
 
-def test_to_polars(repo: Repo) -> None:
+def test_to_polars(repo: Repo) -> None:  # noqa: F811
     """Test conversion to Polars DataFrames."""
     for attr, type_ in OBJECT_TYPES_BY_REPO_ATTR.items():
         schema = SCHEMAS_BY_OBJECT_TYPE[type_]
